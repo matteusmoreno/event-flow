5e85f8c4e6b85cf30c3fe64e6a69c764c031701a
Matteus Moreno <matteus_moreno@live.com>
2024-07-01 14:09:02 -0300

    first commit

---
+++ src/main/java/br/com/matteusmoreno/EventFlow/EventFlowApplication.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.EventFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventFlowApplication.class, args);
+	}
+
+}
